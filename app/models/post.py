477e5dcac4b6eed3f363a36546e0dd0e77190a2d
Cam Chandler <camchandler15@gmail.com>
2021-09-10 20:22:49 -0400

    made post model

---
+++ app/models/post.py
@@ -0,0 +1,27 @@
+from .db import db
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    #columns
+    id = db.Column(db.Integer, primary_key= True)
+    content = db.Column(db.Text)
+    userId  = db.Column(db.Integer, db.ForeignKey("users.id"))
+    activityId = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    createdAt = db.Column(db.DateTime , nullable= False)
+    updatedAt = db.Column(db.DateTime , nullable= False)
+    #relationships
+    user = db.relationship('User', back_populates= 'posts')
+    activity= db.relationship('Activity', back_populates= 'posts')
+    comments = db.relationship('Comment', back_populates='post')
+
+
+    def toDict(self):
+        return {
+            "id":self.id,
+            "content":self.content,
+            "userId": self.userId,
+            "user": self.user.name,
+            "type": self.activity.type.name,
+            "mission": self.activity.name
+        }
