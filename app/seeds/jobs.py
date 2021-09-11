89756570972c1a23f68eedd5b24eabfa810b66b5
Cam Chandler <camchandler15@gmail.com>
2021-09-11 19:56:48 -0400

    made seeds for magic and phys dps

--- app/seeds/jobs.py
+++ app/seeds/jobs.py
@@ -1,4 +1,3 @@
-from typing import SupportsAbs
 from app.models import db, Job
 import datetime
 def seed_roles():
@@ -12,9 +11,20 @@ def seed_roles():
 
 
     for tank in tanks:
-        new_tank = Job(name=tank, roleId = 1 )
+        new_tank = Job(name=tank, roleId = 1 , createdAt=datetime.datetime.now(),updatedAt = datetime.datetime.now())
         db.session.add(new_tank)
 
     for support in supports:
-        new_support = Job(name=support, roleId = 2)
+        new_support = Job(name=support, roleId = 2, createdAt=datetime.datetime.now(),updatedAt = datetime.datetime.now())
+        db.session.add(new_support)
+
+    for mdps in magical_dps:
+        new_dps = Job(name=mdps, roleId = 3, createdAt=datetime.datetime.now(),updatedAt = datetime.datetime.now())
+        db.session.add(new_dps)
+
+    for pdps in physical_dps:
+        new_dps = Job(name=pdps, roleId = 4, createdAt=datetime.datetime.now(),updatedAt = datetime.datetime.now())
+        db.session.add(new_dps)
+
+
     db.session.commit()
