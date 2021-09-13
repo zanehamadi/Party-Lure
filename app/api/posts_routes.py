628cb180f912731ad7836c26e03333f3b4628007
Cam Chandler <camchandler15@gmail.com>
2021-09-13 17:47:28 -0400

    added route to add new post and edit post

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -1,5 +1,8 @@
+
+from operator import pos
 from flask import Blueprint, jsonify, session, request
-from app.models import User, Post
+import datetime
+from app.models import db, User, Post
 from flask_login import current_user, login_user, logout_user, login_required
 
 post_routes = Blueprint('posts', __name__)
@@ -14,23 +17,45 @@ def posts():
 @post_routes.route('/', methods=['POST'])
 def new_post():
     data = request.get_json()
-    post_id = data['postId']
+    post_id = None
+    try:
+        post_id = data['postId']
+    except KeyError:
+        print('new post')
     title = data['title']
     content = data['content']
     user_id = data['userId']
     recruit_level = data['recruitLevel']
     activity_id = data['activityId']
-
+    recruit_role = data['recruitRole']
 
 
     if post_id:
         post = Post.query.get(int(post_id))
+
         # a if condition else b
         post.content = content if content else post.content
         post.recruit_level = recruit_level if recruit_level else post.recruit_level
+        post.activity_id = activity_id if activity_id else post.activity_id
+        post.recruit_role = recruit_role if recruit_role else post.recruit_role
+        post.updated_at = datetime.datetime.now()
+        db.session.add(post)
+        db.session.commit()
+        return post.to_dict()
     else:
-        pass
-
+        post = Post(
+            title = title,
+            content = content,
+            user_id = user_id,
+            recruit_level = recruit_level,
+            recruit_role = recruit_role,
+            activity_id = activity_id,
+            created_at=datetime.datetime.now(),
+            updated_at = datetime.datetime.now()
+        )
+        db.session.add(post)
+        db.session.commit()
+        return post.to_dict()
 @post_routes.route('/<int:post_id>')
 # @login_required
 def post(post_id):
