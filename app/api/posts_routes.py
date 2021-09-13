a2a704880012f291f5bec00e57710fc78c6aa3d0
tannerpedretti <tannerpedretti@gmail.com>
2021-09-13 18:38:00 -0400

    added user and post comment routes

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -2,17 +2,19 @@
 from operator import pos
 from flask import Blueprint, jsonify, session, request
 import datetime
-from app.models import db, User, Post
+from app.models import db, User, Post, Comment
 from flask_login import current_user, login_user, logout_user, login_required
 
 post_routes = Blueprint('posts', __name__)
+
 
 @post_routes.route('/')
 def posts():
 
     posts = Post.query.all()
 
-    return {post.id:post.to_dict() for post in posts}
+    return {post.id: post.to_dict() for post in posts}
+
 
 @post_routes.route('/<int:post_id>')
 # @login_required
@@ -20,6 +22,7 @@ def post(post_id):
     post = Post.query.get(post_id)
     print(post.to_dict())
     return post.to_dict()
+
 
 @post_routes.route('/', methods=['POST'])
 def new_post():
@@ -36,7 +39,6 @@ def new_post():
     activity_id = data['activityId']
     recruit_role = data['recruitRole']
 
-
     if post_id:
         post = Post.query.get(int(post_id))
 
@@ -51,18 +53,19 @@ def new_post():
         return post.to_dict()
     else:
         post = Post(
-            title = title,
-            content = content,
-            user_id = user_id,
-            recruit_level = recruit_level,
-            recruit_role = recruit_role,
-            activity_id = activity_id,
+            title=title,
+            content=content,
+            user_id=user_id,
+            recruit_level=recruit_level,
+            recruit_role=recruit_role,
+            activity_id=activity_id,
             created_at=datetime.datetime.now(),
-            updated_at = datetime.datetime.now()
+            updated_at=datetime.datetime.now()
         )
         db.session.add(post)
         db.session.commit()
         return post.to_dict()
+
 
 @post_routes.delete('/<int:post_id>')
 def delete_post(post_id):
@@ -70,3 +73,13 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return 'deleted'
+
+
+# route to get all the comments for a post
+@post_routes.route('/<int:post_id>/comments')
+# @login_required
+def get_post_comments(post_id):
+    comments = Comment.query.filter(Comment.post_id == post_id)
+    if comments:
+        return {comment.id: comment.to_dict() for comment in comments}
+    return "No Comments"
