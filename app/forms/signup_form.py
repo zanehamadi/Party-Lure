5cc495f58e7beb9a4a30015e659f937b727bffe4
Cam Chandler <camchandler15@gmail.com>
2021-09-13 10:13:36 -0400

    edit wtf to have custom fields

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.fields.core import IntegerField, SelectField
+from wtforms.fields.simple import FileField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models import User, Job
 
 
 def user_exists(form, field):
@@ -21,6 +23,11 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    jobs = Job.query.all()
+    image = FileField('profile-pic')
+    job = SelectField('job', choices= [(job.id, job.name) for job in jobs])
+    level = IntegerField('level')
+    
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
