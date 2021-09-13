0d87a2ad982cee08aed70d3504c3ab436ade333f
zaneh <zanehamadi@gmail.com>
2021-09-13 14:52:02 -0400

    made a users store

---
+++ react-app/src/store/users.js
@@ -0,0 +1,30 @@
+
+const LOAD_USERS = "users/LOAD_USERS"
+
+
+const loadUsers = (users) => ({
+    type: LOAD_USERS,
+    users
+});
+
+export const getUsers = () => async (dispatch) => {
+    const res = await fetch('/api/users');
+    if(res.ok){
+        const users = await res.json();
+        dispatch((loadUsers(users)))
+    }
+};
+
+const initialState = {}
+
+const userReducer = (state = initialState, action ) => {
+    switch(action.type){
+        case LOAD_USERS: {
+            return {...action.users}
+        }
+        default:
+            return state
+    }
+}
+
+export default userReducer
