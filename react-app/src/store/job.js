3f7a1487a1b4c14c71e297762fce6b007d9f3dac
Cam Chandler <camchandler15@gmail.com>
2021-09-13 09:33:18 -0400

    added thunk to get all jobs

---
+++ react-app/src/store/job.js
@@ -0,0 +1,31 @@
+const LOAD_JOBS = 'jobs/load'
+
+const loadJobs = (jobs) => ({
+    type: LOAD_JOBS,
+    jobs
+})
+
+export const getAllJobs = () => async (dispatch) => {
+    const response = await fetch('/api/jobs')
+    let data
+
+    if(response.ok){
+        data = await response.json()
+        console.log('data', data)
+        dispatch(loadJobs(data))
+        return
+    }
+
+    return
+
+}
+const intialState = {}
+
+export default function reducer(state = intialState, action) {
+    switch(action.type){
+        case LOAD_JOBS:
+            return {...action.jobs}
+        default:
+            return state
+    }
+}
