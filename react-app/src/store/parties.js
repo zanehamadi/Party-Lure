de06a33ade74a484020489d7247c33723f8f648e
zaneh <zanehamadi@gmail.com>
2021-09-13 18:01:30 -0400

    setup redux store for parties

---
+++ react-app/src/store/parties.js
@@ -0,0 +1,30 @@
+
+const LOAD_PARTIES = "parties/LOAD_PARTIES"
+
+const loadParties = (parties) => ({
+    type: LOAD_PARTIES,
+    parties
+});
+
+export const getParties = () => async (dispatch) => {
+    const res = await fetch('/api/parties')
+    if(res.ok){
+        const parties = await res.json();
+        dispatch((loadParties(parties)))
+    }
+};
+
+const initialState = {}
+
+const partyReducer = () => (state = initialState, action) => {
+    switch(action.parties){
+        case LOAD_PARTIES: {
+            return {...action.parties}
+        }
+        default:
+            return state
+    }
+}
+
+
+export default partyReducer
