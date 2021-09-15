2a32fbe99628d17bc072ab7bb522e4aef15d9176
zaneh <zanehamadi@gmail.com>
2021-09-15 18:03:35 -0400

    comment validation

--- react-app/src/components/Comments/commentForm.js
+++ react-app/src/components/Comments/commentForm.js
@@ -11,12 +11,26 @@ const CreateCommentForm = ({post}) => {
     const dispatch = useDispatch();
     const history = useHistory();
     const [content, setContent] = useState("");
+    const [validations, setValidations] = useState([])
     const updateComment = (e) => setContent(e.target.value);
 
 
-    const handleSubmit = async (e) => {
+    useEffect(() => {
+
+        let valid = []
         
+        if(content.length > 200) valid.push('Comment too long(200 character limit.)')
+        if(content.length <= 0) valid.push('Please add a valid comment(Comment too short)')
+        setValidations(valid)
+
+
+    }, [content])
+
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setContent("")
         const payload = {
             content,
             user_id:userId,
@@ -25,22 +39,27 @@ const CreateCommentForm = ({post}) => {
         let createdComment = await dispatch(createNewComment(payload))
         if (createdComment) {
             history.push(`/comments`);
-            reset()
         }
+
     };
 
-    const reset = () => {
-        setContent("")
-    }
 
     return (
         <form  onSubmit={handleSubmit} hidden={false}>
+            {validations ? 
+                <>
+                    <ul>
+                        {validations.map(validation => <li>{validation}</li>)}
+                    </ul>
+                </> 
+            : 
+            <></>}
             <input
                 type="text"
                 placeholder="Type comment here"
                 value={content}
                 onChange={updateComment} />
-            <button className="edit-btn edit5" type="submit">Create Comment</button>
+            <button className="edit-btn edit5" type="submit" disabled={validations.length > 0}>Create Comment</button>
         {/* <button type="button" onClick={handleCancelClick}>Cancel</button> */}
     </form>
     )
