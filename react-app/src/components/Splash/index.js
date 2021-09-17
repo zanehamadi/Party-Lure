1cfb0872e5da4c2984e27477c336144c4567a258
zaneh <zanehamadi@gmail.com>
2021-09-17 19:18:23 -0400

    added splash page

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./splash.css"
 
 function Splash() {
     return (
-        <h1>New to Party Lure? Give us money!</h1>
+        <div className="splashContainer">
+            <h1 id="splashTitle">Party Lure</h1>
+            <h2 id="splashSub">The best party finder for the best MMO</h2>
+            <div className="splashLinkDiv">
+                <Link to="/sign-up" className="splashLinks">Sign up</Link> 
+                <Link to="login" className="splashLinks">Log in</Link>
+            </div>
+        </div>
     )
 }
 
