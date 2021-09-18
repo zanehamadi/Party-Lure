1404a3ddb478d02606a1931271304a9b7207ec04
zaneh <zanehamadi@gmail.com>
2021-09-17 20:18:28 -0400

    changed button text color

--- react-app/src/components/Button/ButtonStyle.js
+++ react-app/src/components/Button/ButtonStyle.js
@@ -6,7 +6,7 @@ const ButtonStyle = styled.div`
     margin: 3%;
     border-radius: 5px;
     font-weight: bold;
-    color: rgb(191, 191, 191);
+    color: white;
     padding: .3em .7em;
     outline: none;
     border: none;
