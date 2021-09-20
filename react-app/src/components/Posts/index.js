31aafa105a8fe72966b6041be6cafcabc02a3fb5
Kristian <kristian.panku@gmail.com>
2021-09-19 19:25:05 -0600

    changed text capitals and removed consolelog

--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -5,13 +5,14 @@ import './PostsPage.css'
 export default function Posts({ posts }) {
     return (
         <>
-            <div className = 'posts-container'>
-                {posts.map(post =>{
-                    if(post.comments){
-                        console.log(post, 'postpostpost')
-                    return(
-                    <PostDetails post = {post} />)}
-                    })}
+            <div className='posts-container'>
+                {posts.map(post => {
+                    if (post.comments) {
+
+                        return (
+                            <PostDetails post={post} />)
+                    }
+                })}
             </div>
         </>
     )
