1150f5a65b179d2367ecde8d3ca64d90356ebf72
Cam Chandler <camchandler15@gmail.com>
2021-10-19 09:39:21 -0400

    move requests to own router

---
+++ app/api/friend_request_routes.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, request
+
+from app.models import User, Comment, db, FriendRequest, friend_request
+
+friend_request_routes = Blueprint('requests', __name__)
+
+
+@friend_request_routes.route('/user/<int:id>//received')
+def received_requests(id):
+    friend_requests = FriendRequest.query.filter(FriendRequest.receiver_id == id)
+
+
+    return {received_request.id: received_request.to_dict() for received_request in friend_requests}
+
+
+@friend_request_routes.route('/user/<int:id>/sent')
+def sent_requests(id):
+    friend_requests = FriendRequest.query.filter(FriendRequest.sender_id == id)
+
+    return {sent_request.id: sent_request.to_dict() for sent_request in friend_requests}
+
+
+@friend_request_routes.route('/send', methods = ['POST'])
+def send_request():
+    data = request.get_json()
+
+    sender_id = data['sender_id']
+
+    receiver_id = data['receiver_id']
+
+
+    print('sender', sender_id, 'rec', receiver_id)
+
+    new_friend_request = FriendRequest(
+        sender_id = sender_id,
+        receiver_id = receiver_id
+    )
+    db.session.add(new_friend_request)
+
+    db.session.commit()
+
+    return new_friend_request.to_dict()
+
+
+@friend_request_routes.route('/<int:id>/accept', methods = ['POST'])
+def accept_request(id):
+    data = request.get_json()
+
+    sender_id = data['sender_id']
+    receiver_id = data['receiver_id']
+
+    friend_request = FriendRequest.query.get(id)
+    user = User.query.get(receiver_id)
+
+    user.make_friend(sender_id)
+
+    db.session.add(user)
+    db.session.delete(friend_request)
+    db.session.commit()
+    return {'deleted':id}
+
+
+@friend_request_routes.route('/<int:id>/delete', methods = ['DELETE'])
+def delete_request(id):
+    friend_request = FriendRequest.query.get(id)
+
+    db.session.delete(friend_request)
+    db.session.commit()
+    return {'deleted': id}
--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-from boto3 import session
+
 from app.api.aws import public_file_upload
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
@@ -65,56 +65,65 @@ def edit_user(user_id):
     return user.to_dict()
 
 
-@user_routes.route('/<int:id>/requests/received')
-def received_requests(id):
-    friend_requests = FriendRequest.query.filter(FriendRequest.receiver_id == id)
+# @user_routes.route('/<int:id>/requests/received')
+# def received_requests(id):
+#     friend_requests = FriendRequest.query.filter(FriendRequest.receiver_id == id)
 
 
-    return {received_request.id: received_request.to_dict() for received_request in friend_requests}
+#     return {received_request.id: received_request.to_dict() for received_request in friend_requests}
 
 
-@user_routes.route('/<int:id>/requests/sent')
-def sent_requests(id):
-    friend_requests = FriendRequest.query.filter(FriendRequest.sender_id == id)
+# @user_routes.route('/<int:id>/requests/sent')
+# def sent_requests(id):
+#     friend_requests = FriendRequest.query.filter(FriendRequest.sender_id == id)
 
-    return {sent_request.id: sent_request.to_dict() for sent_request in friend_requests}
-
-
-@user_routes.route('/requests/send', methods = ['POST'])
-def send_request():
-    data = request.get_json()
-
-    sender_id = data['sender_id']
-
-    receiver_id = data['receiver_id']
+#     return {sent_request.id: sent_request.to_dict() for sent_request in friend_requests}
 
 
-    print('sender', sender_id, 'rec', receiver_id)
+# @user_routes.route('/requests/send', methods = ['POST'])
+# def send_request():
+#     data = request.get_json()
 
-    new_friend_request = FriendRequest(
-        sender_id = sender_id,
-        receiver_id = receiver_id
-    )
-    db.session.add(new_friend_request)
+#     sender_id = data['sender_id']
 
-    db.session.commit()
-
-    return new_friend_request.to_dict()
+#     receiver_id = data['receiver_id']
 
 
-@user_routes.route('/request/<int:id>/accept', methods = ['POST'])
-def accept_request(id):
-    data = request.get_json()
+#     print('sender', sender_id, 'rec', receiver_id)
 
-    sender_id = data['sender_id']
-    receiver_id = data['receiver_id']
+#     new_friend_request = FriendRequest(
+#         sender_id = sender_id,
+#         receiver_id = receiver_id
+#     )
+#     db.session.add(new_friend_request)
 
-    friend_request = FriendRequest.query.get(id)
-    user = User.query.get(receiver_id)
+#     db.session.commit()
 
-    user.make_friend(sender_id)
+#     return new_friend_request.to_dict()
 
-    db.session.add(user)
-    db.session.delete(friend_request)
-    db.session.commit()
-    return {'deleted':id}
+
+# @user_routes.route('/request/<int:id>/accept', methods = ['POST'])
+# def accept_request(id):
+#     data = request.get_json()
+
+#     sender_id = data['sender_id']
+#     receiver_id = data['receiver_id']
+
+#     friend_request = FriendRequest.query.get(id)
+#     user = User.query.get(receiver_id)
+
+#     user.make_friend(sender_id)
+
+#     db.session.add(user)
+#     db.session.delete(friend_request)
+#     db.session.commit()
+#     return {'deleted':id}
+
+
+# @user.route('/request/<int:id>', methods = ['DELETE'])
+# def delete_request(id):
+#     friend_request = FriendRequest.query.get(id)
+
+#     db.session.delete(friend_request)
+#     db.session.commit()
+#     return {'deleted': id}
