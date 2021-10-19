a98b9772f8246176c13ea325b0e144efa4544e56
Cam Chandler <camchandler15@gmail.com>
2021-10-19 11:08:54 -0400

    create friend request reducer

---
+++ react-app/src/store/friend_requests.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+const initialState = {sent: {}, received: {}}
+
+const friendRequestReducer = (state = initialState, action) => {
+
+    switch(action.type){
+        default:
+            return {...state}
+    }
+}
+
+
+export default friendRequestReducer
