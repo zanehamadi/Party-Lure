29ed4c2655dafdd049f83c71bfe4fbd8f0672ecf
Cam Chandler <camchandler15@gmail.com>
2021-10-19 11:25:41 -0400

    thunk to get sent requests

--- react-app/src/store/friend_requests.js
+++ react-app/src/store/friend_requests.js
@@ -1,4 +1,7 @@
 const GET_RECEIVED = 'friendRequests/GET_RECEIVED'
+const GET_SENT = 'friendRequests/GET_SENT'
+
+
 
 
 const getReceived = (requests) => ({
@@ -6,13 +9,29 @@ const getReceived = (requests) => ({
     requests
 })
 
+const getSent = (requests) => ({
+    type: GET_SENT,
+    requests
+})
+
 export const goGetReceived = (user_id) => async(dispatch) => {
-    let res = await fetch('/api/requests/user/${user_id}/received')
+    let res = await fetch(`/api/requests/user/${user_id}/received`)
 
 
     if(res.ok){
         let data  = await res.json()
         dispatch(getReceived(data))
+    }
+
+ }
+
+ export const goGetSent = (user_id) => async(dispatch) => {
+    let res = await fetch(`/api/requests/user/${user_id}/sent`)
+
+
+    if(res.ok){
+        let data  = await res.json()
+        dispatch(getSent(data))
     }
 
  }
@@ -23,6 +42,9 @@ const friendRequestReducer = (state = initialState, action) => {
     switch(action.type){
        case GET_RECEIVED:{
             return {...state, received: {...action.requests}}
+        }
+        case GET_SENT:{
+            return{...state, sent:{...action.requests}}
         }
         default:
             return {...state}
